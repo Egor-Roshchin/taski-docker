d2aff919b3846c905864bad9b5a2a76c02b77034
Egor Roshchin <roshin.study@gmail.com>
2025-06-01 18:45:43 +0300

    Deploy fix ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,10 +8,10 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
-# ALLOWED_HOSTS = [
-#     '158.160.75.96', 'localhost', 'absqure.zapto.org', '127.0.0.1',
-# ]
+# ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [
+    '158.160.75.96', 'localhost', 'absqure.zapto.org', '127.0.0.1',
+]
 
 
 # Application definition
